bb220a0044e8d1c3dbaebc9b0aa592e57a3390c9
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2021-09-24 18:00:54 +0530

    Add files via upload

---
+++ Static/StaticDemo.java
@@ -0,0 +1,38 @@
+
+class StaticDemo
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Value of static j : "+Demo.j);
+		Demo.gun();	// Demo::gun()
+
+		Demo obj = new Demo();
+		obj.fun();
+		obj.gun();
+
+		System.out.println("Value of non static i : "+obj.i);
+		System.out.println("Value of static j : "+obj.j);
+	}
+}
+class Demo
+{
+	public int i = 10;
+	public static int j = 20;
+
+	public void fun()
+	{
+		System.out.println("Inside non static fun");
+		System.out.println(i);
+		System.out.println(j);
+	}
+
+	public static void gun()
+	{
+		System.out.println("Inside static gun");
+		//System.out.println(i); NA
+		System.out.println(j);
+	}
+
+}
+
+// static int Demo :: j = 20;	
