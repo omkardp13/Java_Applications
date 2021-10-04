021651ddde7d4139368632db79bda5407a3f0f75
omkardp13 <56990422+omkardp13@users.noreply.github.com>
2021-10-04 08:56:29 +0530

    Add files via upload

---
+++ Virtual/Virtual.java
@@ -0,0 +1,35 @@
+class Virtual
+{
+	public static void main(String arg[])
+	{
+		Base bp = new Derived();	// Upcasting
+		bp.fun();	// Derived fun
+		bp.gun();	// Base gun
+		// bp.sun();
+	}
+}
+
+class Base
+{
+	public void fun()
+	{
+		System.out.println("Base fun");
+	}
+	public void gun()
+	{
+		System.out.println("Base gun");
+	}
+}
+
+class Derived extends Base
+{
+	public void fun()		// Overriding
+	{
+		System.out.println("Derived fun");
+	}
+	public void sun()
+	{
+		System.out.println("Derived sun");
+	}
+}
+
